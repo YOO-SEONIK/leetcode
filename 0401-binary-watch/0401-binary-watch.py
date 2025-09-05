02ffabcc5f3c0bd5973ee60acfd8563e526ccfc3
YOO SEONIK <dbtjsdlr123@gmail.com>
2025-09-05 20:00:06 +0900

    Time: 0 ms (100%), Space: 17.7 MB (73.92%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        ans = []
+        for h in range(12):
+            for m in range(60):
+                if (h.bit_count() + m.bit_count()) == turnedOn:  # Py3.8+
+                    ans.append(f"{h}:{m:02d}")
+        return ans
+        
