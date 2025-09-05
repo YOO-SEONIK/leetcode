fb1f84cda0fc41458d156f4ba4146b654460e9f0
YOO SEONIK <dbtjsdlr123@gmail.com>
2025-09-05 20:21:56 +0900

    Time: 4 ms (32.4%), Space: 18 MB (40.29%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        pattern = OrderedDict([i, -1] for i in nums1)
+
+        for i in range(len(nums2) - 1):
+            for j in range(i + 1, len(nums2)):
+                if nums2[i] < nums2[j]:
+                    if nums2[i] in pattern:
+                        pattern[nums2[i]] = nums2[j]
+                    break
+
+        return list(pattern.values())
+        
