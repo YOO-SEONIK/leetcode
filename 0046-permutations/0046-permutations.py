e36c8ac80371ec2780e92d7921ec2e07a8827057
YOO SEONIK <dbtjsdlr123@gmail.com>
2025-12-19 15:24:21 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,16 +1,1 @@
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        result , visited = [], []
-        def dfs(nums):
-            if len(nums) == 0:
-                result.append(visited[:])
-            for num in nums:
-                next = nums[:]
-                next.remove(num)
-                visited.append(num)
-                dfs(next)
-                visited.pop()
-
-        dfs(nums)
-        return result
-        
+wsfwf
